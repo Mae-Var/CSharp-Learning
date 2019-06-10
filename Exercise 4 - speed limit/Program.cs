3ea406c30b386f5d668686fdb785b72113e07c80
B Popov <ashelan@gmail.com>
2019-06-10 12:11:46 +0300

    final fix and push

--- Exercise 4 - speed limit/Program.cs
+++ Exercise 4 - speed limit/Program.cs
@@ -14,8 +14,9 @@ namespace Exercise_4___speed_limit
             Console.WriteLine("Please, enter the speed used by the driver: ");
             inputSpeed = Convert.ToInt32(Console.ReadLine());
 
+            int demeritPerKM = 5;
             int overTheLimit = inputSpeed - speedLimit;
-            int demeritPoints = 5;
+            int demeritPoints = overTheLimit / demeritPerKM;
 
             if (inputSpeed < speedLimit)
             {
@@ -23,7 +24,14 @@ namespace Exercise_4___speed_limit
             }
             else
             {
-                Console.WriteLine("Your speed was " + overTheLimit + "km/h over the limit. This results in " + overTheLimit / demeritPoints + " demerit points.");
+                if (demeritPoints < 12)
+                {
+                    Console.WriteLine("Your speed was " + overTheLimit + "km/h over the limit. This results in " + demeritPoints + " demerit points.");
+                }
+                else
+                {
+                    Console.WriteLine("Your speed was " + overTheLimit + "km/h over the limit. This results in " + demeritPoints + " demerit points.\n*LICENSE SUSPENDED*");
+                }
             }
         }
     }
