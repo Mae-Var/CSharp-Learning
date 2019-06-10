1830699977528321ba3ab840f5c98a8c0f0e0d6f
B Popov <ashelan@gmail.com>
2019-06-10 15:18:05 +0300

    loops commit v x

---
+++ loops - echo code/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace loops___echo_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Type your name: ");
+                string input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue; // continues to the beginning of the loop i.e. to the while (true)
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ while loops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace while_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 0;
+
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+
+                    Console.WriteLine(i);
+
+                i++;
+
+            }
+        }
+    }
+}
