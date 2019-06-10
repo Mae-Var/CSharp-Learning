6d2a0eb31883b27942859768f369bcf8cb0ffe51
B Popov <ashelan@gmail.com>
2019-06-10 11:17:26 +0300

    exercise 1

---
+++ Exercise 1 - 10 to 10 int/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_1___10_to_10_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            Console.WriteLine("Enter a number between 1 and 10:");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            if (input >= 1 && input <= 10)
+            {
+                Console.WriteLine("The number you have entererd is: " + input);
+            }
+            else
+            {
+                Console.WriteLine("Please, make sure you've entered a whole number between 1 and 10.");
+            }
+        }
+    }
+}
