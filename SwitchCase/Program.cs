4d94b2de1e0cf3dbc01f7522da5ad944736c9297
B Popov <ashelan@gmail.com>
2019-06-10 10:41:59 +0300

    case switch update

--- SwitchCase/Program.cs
+++ SwitchCase/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SwitchCase
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            var season = Season.Autumn;
+
+            switch (season) {
+                case Season.Autumn:
+                    Console.WriteLine("It's autumn! It's rainy and beautiful.");
+                    break;
+
+                case Season.Spring:
+                    Console.WriteLine("It's spring. It's rainy and everything is green.");
+                    break;
+
+                case Season.Summer:
+                    Console.WriteLine("It's summer. It is so hot, I wish there was a sea nearby.");
+                    break;
+
+                case Season.Winter:
+                    Console.WriteLine("It's winter time! Beautiful, but cold.");
+                    break;
+            }
         }
     }
 }
