29df0b0a4567b116eee8f5ed5dd211a457322bd2
B Popov <ashelan@gmail.com>
2019-06-10 16:17:28 +0300

    exercise 1-100 divisble by 3

---
+++ exercise - 1 to 100 divisible by 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise___1_to_100_divisible_by_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            for (var i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                    counter++;
+            }
+            Console.WriteLine("The total of numbers divisble by 3 between 1-100 is: {0}", counter);
+        }
+    }
+}
