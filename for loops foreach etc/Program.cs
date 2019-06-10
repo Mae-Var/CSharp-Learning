491683956aadaf28c7dd6fa1700cf5e245484d79
B Popov <ashelan@gmail.com>
2019-06-10 14:50:39 +0300

    learning loops commit

---
+++ for loops foreach etc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace for_loops_foreach_etc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ for loops v2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace for_loops_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "John Wick";
+
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            foreach (var character in name) // same as the above, but with foreach method
+            {
+                Console.WriteLine(character);
+            }
+        }
+    }
+}
