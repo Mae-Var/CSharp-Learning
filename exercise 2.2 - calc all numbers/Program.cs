9569eb64edd7247f02fc1dea28db47bf801da54e
B Popov <ashelan@gmail.com>
2019-06-13 11:56:39 +0300

    2.2 exercise added

---
+++ exercise 2.2 - calc all numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercise_2._2___calc_all_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Add a valid number or 'ok' to exit: ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                break;
+
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("The total of all accumulated numbers is: " + sum);
+        }
+    }
+}
