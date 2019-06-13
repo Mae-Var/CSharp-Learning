b416ec1b9f80e14950c03c8064fb1eca6ca0931d
B Popov <ashelan@gmail.com>
2019-06-13 12:45:04 +0300

    exercise 3.3 commit

---
+++ exercise 3.3 - compute factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise_3._3___compute_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a valid number to see it's factorial: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int sum = 1;
+
+            for (int i = 1; i <= input; i++)
+            {
+                sum *= i;
+            }
+            Console.WriteLine("The total of all accumulated numbers is: " + sum);
+
+        }
+
+    }
+}
