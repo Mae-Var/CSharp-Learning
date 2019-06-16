2f17c304b54645f3a7bda7dc4a82ea3b0dace89b
B Popov <ashelan@gmail.com>
2019-06-16 19:08:03 +0300

    commit sum ting

--- exercise - 1 to 100 divisible by 3/Program.cs
+++ exercise - 1 to 100 divisible by 3/Program.cs
@@ -4,15 +4,22 @@ namespace exercise___1_to_100_divisible_by_3
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string DevisibleByThree()
         {
-            var counter = 0;
+            int counter = 0;
+            string ret1 = "The total of numbers divisble by 3 between 1-100 is ";
             for (var i = 1; i < 100; i++)
             {
                 if (i % 3 == 0)
                     counter++;
             }
-            Console.WriteLine("The total of numbers divisble by 3 between 1-100 is {0}", counter);
+            return ret1 + counter + ".";
         }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DevisibleByThree());
+        }
+
     }
 }
