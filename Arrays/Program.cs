1b23723e9eefc14a4479d1588fbc7bf9b034afc8
B Popov <ashelan@gmail.com>
2019-06-18 14:11:20 +0300

    commit arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 3, 7, 9, 2, 14, 6 };
+            var numbers2 = new[] { 3, 7, 9, 2, 14, 6 }; // another way to create an Array;
+
+            // Lenght
+            Console.WriteLine("Lenght of the array is: " + numbers.Length);
+
+            //IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("The index of '9' is: " + index);
+
+            //Clear()
+            Array.Clear(numbers, 0, 2); //Clear accepts 3 parameters, 1st is the Array, 2nd is the "starting index", 3rd is the number of elements you wish to be cleared;
+            Console.WriteLine("Effect of Clear(): ");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n); // if it's an array of boolean, it will set them to false; if it's an array of strings, it will set them to null;
+            };
+
+            //Copy()
+            int[] anotherArr = new int[3];
+            Array.Copy(numbers2, anotherArr, 3); // 1st param source array, 2nd is destination, 3rd is number of elements to be copied;
+            Console.WriteLine("Effect of Copy(): ");
+            foreach (var m in anotherArr)
+            {
+                Console.WriteLine(m);
+            }
+
+            //Sort()
+            Array.Sort(numbers);
+
+            Console.WriteLine("Effect of Sort(): ");
+            foreach (var z in numbers)
+            {
+                Console.WriteLine(z);
+            }
+
+            //Reverse()
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Reverse(): ");
+            foreach (var x in numbers)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ Matrix and Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Matrix_and_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new int[3, 5];
+
+            var ma3x = new int[3, 6] { // Rectangular Array;
+                { 1, 2, 3, 4, 5, 6 },
+                { 1, 2, 3, 4, 5, 6 },
+                { 1, 2, 3, 4, 5, 6 }
+            };
+
+            var array = new int[3][]; // Jagged Array; in the first [] we specify the number of elements in top-level array i.e. 3 arrays with [] sub-arrays;
+            array[0] = new int[4];
+            array[1] = new int[5];
+            array[2] = new int[3];
+
+
+
+        }
+    }
+}
