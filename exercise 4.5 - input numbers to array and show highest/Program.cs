e5c464a0272e23107dd1b7bffebd5b864db39ae9
B Popov <ashelan@gmail.com>
2019-06-18 13:03:25 +0300

    exercise 4.5 commit

---
+++ exercise 4.5 - input numbers to array and show highest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercise_4._5___input_numbers_to_array_and_show_highest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter comma separated numbers: ");
+
+            string input = Console.ReadLine();
+            var numbers = input.Split(',');
+            int max = Convert.ToInt32(numbers[0]);
+
+            foreach (var digit in numbers)
+            {
+                var number = Convert.ToInt32(digit);
+                if (number > max)
+                {
+                    max = number;
+                }
+            };
+            Console.WriteLine("Max number is: " + max);
+        }
+    }
+}
