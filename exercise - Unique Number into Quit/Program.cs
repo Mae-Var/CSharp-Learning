f94887083d8542ad30ec734b531968f21276ed04
B Popov <ashelan@gmail.com>
2019-06-24 14:42:18 +0300

    update with more readable code

--- exercise - Unique Number into Quit/Program.cs
+++ exercise - Unique Number into Quit/Program.cs
@@ -11,23 +11,33 @@ namespace exercise___Unique_Number_into_Quit
         static void Main(string[] args)
         {
             List<int> listOfNums = new List<int>();
-            List<int> uniqueNums = new List<int>();
+            
 
             // Console.WriteLine("Please, add numbers and once you want to see the result, type in 'quit':");
 
-            Console.Write("Enter a number or 'quit' to exit and see all the unique numbers: ");
+            Console.Write("Enter a number or 'Quit' to exit and see all the unique numbers: ");
 
             while (true)
             {
-                Console.Write("Enter next number or 'quit': ");
+                Console.Write("Enter next number or 'Quit': ");
                 var input = Console.ReadLine();
 
                 if (input.ToLower() == "quit")
                 {
-                    uniqueNums = listOfNums.Distinct().ToList();
-                    foreach (int num in uniqueNums)
+                    Console.WriteLine("Below are all unique numbers:");
+
+                    var uniques = new List<int>();
+                    foreach (int num in listOfNums)
                     {
-                        Console.WriteLine("This is one of the unique numbers: {0}", num);
+                        if (!uniques.Contains(num))
+                        {
+                            uniques.Add(num);
+                        }
+                    }
+
+                    foreach (int num in uniques)
+                    {
+                        Console.WriteLine(num);
                     }
                     break;
                 }
@@ -36,3 +46,6 @@ namespace exercise___Unique_Number_into_Quit
         }
     }
 }
+
+// List<int> uniqueNums = new List<int>();
+// uniqueNums = listOfNums.Distinct().ToList(); // this is the fastest way, but you need to know how to do it without this method.
