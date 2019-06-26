f8a5203c7e77bb6de05f5e7c8abc483b765f6721
B Popov <ashelan@gmail.com>
2019-06-26 10:38:55 +0300

    update on strings

--- strings in depth/Program.cs
+++ strings in depth/Program.cs
@@ -10,6 +10,20 @@ namespace strings_in_depth
     {
         static void Main(string[] args)
         {
+            string fullName = "Boyan Popov ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim()); // removes the blank space in the end of the name.
+            Console.WriteLine("ToUpper: '{0}'", fullName.ToUpper());
+            Console.WriteLine("ToLower: '{0}'", fullName.ToLower());
+
+            var index = fullName.IndexOf(' '); // tracks index of blank space
+            string firstName = fullName.Substring(0, index); //takes 1st part of the fullName till it reaches index (i.e. blank space)
+            string lastName = fullName.Substring(index + 1); // takes the part after blank space.
+            Console.WriteLine("First name: " + firstName);
+            Console.WriteLine("Last name: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("First name after .Split: " + names[0]);
+            Console.WriteLine("Last name after .Split: " + names[1]);
         }
     }
 }
