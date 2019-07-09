59d93ce2686bf7892c0e53f84d35e9ecdf0e5ccf
B Popov <ashelan@gmail.com>
2019-07-09 14:36:43 +0300

    working

--- Lemberg 2nd exercise/Program.cs
+++ Lemberg 2nd exercise/Program.cs
@@ -10,49 +10,56 @@ namespace Lemberg_2nd_exercise
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Please input one of the below commands: ");
-            Console.WriteLine("1) T or Time to get current time of the day.");
-            Console.WriteLine("2) D or Date to get current date of the year.");
-            Console.WriteLine("3) S or Sum to summarize 2 numbers.");
-            Console.WriteLine("4) Q or Quit to exit.");
-
-            string input = Console.ReadLine().ToLower();
-
-            if (input == "t" || input == "time")
+            while (true)
             {
-                string timeNow = DateTime.Now.ToString("hh:mm tt");
-                Console.WriteLine("The time is: " + timeNow);
+                Console.WriteLine("\nPlease input one of the below commands: ");
+                Console.WriteLine("1) T or Time to get current time of the day.");
+                Console.WriteLine("2) D or Date to get current date of the year.");
+                Console.WriteLine("3) S or Sum to summarize 2 numbers.");
+                Console.WriteLine("4) Q or Quit to exit.\n");
 
-            }
-            else if (input == "d" || input == "date")
-            {
-                string dateNow = DateTime.Now.ToString("yyyy-MMM-dd");
-                Console.WriteLine("The date is: " + dateNow);
+                string input = Console.ReadLine().ToLower();
 
-            }
-            else if (input == "s" || input == "sum")
-            {
-                Console.WriteLine("Please, enter first number: ");
-                int one = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Please, enter second number: ");
-                int two = Convert.ToInt32(Console.ReadLine());
+                if (input == "t" || input == "time")
+                {
+                    string timeNow = DateTime.Now.ToString("hh:mm tt");
+                    Console.WriteLine("The time is: " + timeNow);
+                }
+                else if (input == "d" || input == "date")
+                {
+                    string dateNow = DateTime.Now.ToString("yyyy-MMM-dd");
+                    Console.WriteLine("The date is: " + dateNow);
+                }
+                else if (input == "s" || input == "sum")
+                {
+                    Console.WriteLine("Please, enter 2 numbers: ");
+                    int one = NumCheck();
+                    int two = NumCheck();
 
-                int sum = one + two;
-
-                Console.WriteLine("The sum is: {0}.", sum);
+                    Console.WriteLine("The sum is: {0}.", Sum(one, two));
+                }
+                else if (input == "q" || input == "quit")
+                {
+                    Console.WriteLine("Leaving the console...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You have used an unknown command - please, use a valid one.");
+                }
             }
-            else if (input == "q" || input == "quit")
-            {
-                Console.WriteLine("Leaving the console...");
-                return;
-            }
-            else
-            {
-                Console.WriteLine("You have used an unknown command - exiting now...");
-                return;
-            }
-
+        }
+        private static int NumCheck()
+        {
+            int x;
+            Console.Write("Enter two valid numbers (int): ");
+            while (!int.TryParse(Console.ReadLine(), out x))
+            Console.Write("This is not a number, try again: ");
+            return x;
+        }
+        static int Sum(int a, int b)
+        {
+            return a + b;
         }
     }
 }
